dd0e054687472f69947e8e16f3509da7fe479a45
stacyvana <ssvana83@yahoo.com>
2022-09-14 12:44:47 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', handleSubmit)
+  
 });
+
+function handleSubmit(event) {
+  event.preventDefault();
+  
+
+  const taskList = document.getElementById('tasks');
+  const task = document.createElement('li');
+  task.textContent = event.target.description.value;
+  taskList.append(task);
+}
